596d3e33abde70716279606a85861ce1e355b7b6
Sergio Clavijo <sergio.clavijo@wunderman.com>
2021-09-29 12:26:42 -0500

    Ajustada visualización del formulario de servicios, agregado js para submit del formulario...

---
+++ productos.js
@@ -0,0 +1,7 @@
+const btnCrearServicio = document.getElementById('btnCrearServicio');
+
+btnCrearServicio.addEventListener('click', function (evento) {
+    evento.preventDefault();
+
+    alert('El servicio se ha guardado correctamente');
+});
