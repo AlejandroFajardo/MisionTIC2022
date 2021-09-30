17c9f526e7034fead75e0a6ec76c6fd429a4d40f
Sergio Clavijo <sergio.clavijo@wunderman.com>
2021-09-29 22:12:25 -0500

    Avances en la funcionalidad de editar servicios en el formulario de ventas...

--- ventas.js
+++ ventas.js
@@ -3,10 +3,18 @@ const txtServicio = document.getElementById('Servicio');
 const txtCantidad = document.getElementById('Cantidad');
 const txtValorUnitario = document.getElementById('ValorUnitario');
 const btnGuardarServicio = document.getElementById('btnGuardarServicio');
-const modalServicios = bootstrap.Modal.getOrCreateInstance(document.getElementById('ModalServicios')); //Obtener el objeto de Bootstrap Modal, para poder hacer uso de funciones de utilidad
+const modal = document.getElementById('ModalServicios');
+const modalServicios = bootstrap.Modal.getOrCreateInstance(modal); //Obtener el objeto de Bootstrap Modal, para poder hacer uso de funciones de utilidad
 const tableServicios = document.getElementById('Servicios');
 const tbody = tableServicios.querySelector('tbody');
 const formNuevoServicio = document.getElementById('FormNuevoServicio');
+const tituloModal = document.getElementById('tituloModal');
+const btnServicios = document.getElementById('btnServicios');
+
+//Declaración de variables
+let arrayServicios = [];
+let editandoServicio = false;
+let botonEditandoServicio = null;
 
 //Evento click del botón
 btnGuardarServicio.addEventListener('click', function (evento) {
@@ -16,49 +24,110 @@ btnGuardarServicio.addEventListener('click', function (evento) {
     let valorUnitario = parseInt(txtValorUnitario.value);
 
     if (cantidad > 0) {
-        let fila = document.createElement('tr'); //Creación de la fila
-        let colIdServicio = document.createElement('td'); //Creación de columna
-        colIdServicio.innerHTML = '';
-        let colServicio = document.createElement('td');
-        colServicio.innerHTML = nombreServicio;
-        let colCantidad = document.createElement('td');
-        colCantidad.innerHTML = cantidad;
-        let colValorUnitario = document.createElement('td');
-        colValorUnitario.innerHTML = valorUnitario;
-        let colValorTotal = document.createElement('td');
-        colValorTotal.innerHTML = valorUnitario * cantidad;
-        let colEditar = document.createElement('td');
-        colEditar.innerHTML = '';
-        let colBorrar = document.createElement('td');
-        //colBorrar.innerHTML = '<button type="button" class="btn btn-danger">X</button>'; //No recomendado
-        let btnBorrar = document.createElement('button'); //Estandar y recomendada!!
-        btnBorrar.textContent = 'X';
-        btnBorrar.type = 'button';
-        btnBorrar.classList.add('btn');
-        btnBorrar.classList.add('btn-danger');
-        btnBorrar.classList.add('borrar-fila');
-        btnBorrar.addEventListener('click', borrarFila); //Agregamos evento click del botón de eliminar la fila
-        colBorrar.appendChild(btnBorrar); //Asociar la fila a la columna de borrar
+        if (editandoServicio) {
+            let indice = botonEditandoServicio.dataset.indice;
+            arrayServicios[indice].nombreServicio = nombreServicio;
+            arrayServicios[indice].cantidad = cantidad;
+            arrayServicios[indice].valorUnitario = valorUnitario;
 
-        //Asociar las columnas a la fila
-        fila.appendChild(colIdServicio);
-        fila.appendChild(colServicio);
-        fila.appendChild(colCantidad);
-        fila.appendChild(colValorUnitario);
-        fila.appendChild(colValorTotal);
-        fila.appendChild(colEditar);
-        fila.appendChild(colBorrar);
+            let fila = botonEditandoServicio.closest('tr');
+            fila.children[1].innerText = nombreServicio;
+            fila.children[2].innerText = cantidad;
+            fila.children[3].innerText = valorUnitario;
+            fila.children[4].innerText = valorUnitario * cantidad;
 
-        tbody.appendChild(fila); //Agregar la nueva fila al tbody de la tabla
+            editandoServicio = false;
+            indiceServicioEditando = null;
+            formNuevoServicio.reset();
 
-        formNuevoServicio.reset(); //Resetea (borra) todos los valores del formulario
+            //modalServicios.hide(); //Oculta el modal
+        } else {
+            arrayServicios.push({
+                nombreServicio: nombreServicio,
+                cantidad: cantidad,
+                valorUnitario: valorUnitario
+            });
+            let fila = document.createElement('tr'); //Creación de la fila
+            let colIdServicio = document.createElement('td'); //Creación de columna
+            colIdServicio.innerHTML = '';
+            let colServicio = document.createElement('td');
+            colServicio.innerHTML = nombreServicio;
+            let colCantidad = document.createElement('td');
+            colCantidad.innerHTML = cantidad;
+            let colValorUnitario = document.createElement('td');
+            colValorUnitario.innerHTML = valorUnitario;
+            let colValorTotal = document.createElement('td');
+            colValorTotal.innerHTML = valorUnitario * cantidad;
 
-        modalServicios.hide(); //Oculta el modal
+
+            let colEditar = document.createElement('td');
+            let btnEditar = document.createElement('button');
+            btnEditar.textContent = 'editar';
+            btnEditar.type = 'button';
+            btnEditar.classList.add('btn');
+            btnEditar.classList.add('btn-info');
+            btnEditar.classList.add('editar-fila');
+            btnEditar.dataset.indice = arrayServicios.length - 1;
+            btnEditar.addEventListener('click', editarFila);
+            colEditar.appendChild(btnEditar);
+
+
+            let colBorrar = document.createElement('td');
+            //colBorrar.innerHTML = '<button type="button" class="btn btn-danger">X</button>'; //No recomendado
+            let btnBorrar = document.createElement('button'); //Estandar y recomendada!!
+            btnBorrar.textContent = 'X';
+            btnBorrar.type = 'button';
+            btnBorrar.classList.add('btn');
+            btnBorrar.classList.add('btn-danger');
+            btnBorrar.classList.add('borrar-fila');
+            btnBorrar.dataset.indice = arrayServicios.length - 1;
+            btnBorrar.addEventListener('click', borrarFila); //Agregamos evento click del botón de eliminar la fila
+            colBorrar.appendChild(btnBorrar); //Asociar la fila a la columna de borrar
+
+            //Asociar las columnas a la fila
+            fila.appendChild(colIdServicio);
+            fila.appendChild(colServicio);
+            fila.appendChild(colCantidad);
+            fila.appendChild(colValorUnitario);
+            fila.appendChild(colValorTotal);
+            fila.appendChild(colEditar);
+            fila.appendChild(colBorrar);
+
+            tbody.appendChild(fila); //Agregar la nueva fila al tbody de la tabla
+
+            formNuevoServicio.reset(); //Resetea (borra) todos los valores del formulario
+
+            //modalServicios.hide(); //Oculta el modal
+        }
     }
+});
+
+btnServicios.addEventListener('click', function () {
+    tituloModal.innerText = 'Agregar servicio';
+    formNuevoServicio.reset(); //Límpia los campos
+    editandoServicio = false;
 });
 
 function borrarFila() {
     if (confirm('¿Está seguro de borrar este servicio?')) {
+        arrayServicios.splice(this.dataset.indice, 1);
         this.parentNode.parentNode.remove(); //Eliminar la fila
+
+        //Actualizar índice de botones siguientes
+        let botones = document.getElementsByClassName('borrar-fila');
+        for (let i = 0; i < botones.length; i++) {
+            botones[i].dataset.indice = i;
+        }
     }
+}
+
+function editarFila() {
+    editandoServicio = true;
+    botonEditandoServicio = this;
+    tituloModal.innerText = 'Editando servicio';
+    let datos = arrayServicios[this.dataset.indice];
+    txtServicio.value = datos.nombreServicio;
+    txtCantidad.value = datos.cantidad;
+    txtValorUnitario.value = datos.valorUnitario;
+    modalServicios.show();
 }
