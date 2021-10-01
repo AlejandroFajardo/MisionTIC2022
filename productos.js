18b4feb028cb7e5216f1c121515194b7209f829d
isabel cristina <87945871+IsaRave@users.noreply.github.com>
2021-10-01 16:48:12 -0500

    Revert "Development"

--- productos.js
+++
@@ -1,7 +0,0 @@
-const btnCrearServicio = document.getElementById('btnCrearServicio');
-
-btnCrearServicio.addEventListener('click', function (evento) {
-    evento.preventDefault();
-
-    alert('El servicio se ha guardado correctamente');
-});
