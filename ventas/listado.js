09982c86e9d814e2d3eeec7bf898bc86e47683c8
Sergio Clavijo <sergio.clavijo@wunderman.com>
2021-10-03 20:49:34 -0500

    Actualizado diseño del listado de ventas.

--- ventas/listado.js
+++ ventas/listado.js
@@ -16,22 +16,3 @@ function myFunction() {
       }       
     }
   }
-  
-  function myFunction_1() {
-    var input, filter, table, tr, td, i, txtValue;
-    input = document.getElementById("myInput1");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("myTable");
-    tr = table.getElementsByTagName("tr");
-    for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[2];
-      if (td) {
-        txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-        } else {
-          tr[i].style.display = "none";
-        }
-      }       
-    }
-  }
