2d074a49180712cd593f7e68856f3cbb1395ae1b
rusbell ruiz <rusbell.ruiz.p@gmail.com>
2021-10-03 19:34:16 -0500

    filtro por id funcional en buscar.html

--- servicios/buscar.js
+++ servicios/buscar.js
@@ -15,9 +15,65 @@ const getServicios = () => db.collection('servicios').get();
 
 const buscarservicio = (id) => db.collection("servicios").doc(id).log("si");
 
-// window.addEventListener("DOMContentLoaded", async(e) =>{
-    
-// })
+window.addEventListener("DOMContentLoaded", async(e) =>{
+    const services = await getServicios()
+    services.forEach(doc =>{
+        const idservicio = doc.id;
+        const nombreServicio = doc.data().servicio;
+        const descripcionServicio = doc.data().descripcion;
+        const cantidadServicio = doc.data().cantidad;
+        const valorUniServicio = doc.data().valorUnitario;
+        const valorTotalServicio = doc.data().valorTotal;
+        console.log(nombreServicio);
+        console.log(descripcionServicio);
+        console.log(cantidadServicio);
+        console.log(valorUniServicio);
+        console.log(valorTotalServicio);
+        let fila = document.createElement('tr');
+        let colIdServicio= document.createElement('td');
+        colIdServicio.innerHTML= idservicio;
+        let colServicio= document.createElement('td');
+        colServicio.innerHTML = nombreServicio;
+        let colDescripcion= document.createElement('td');
+        colDescripcion.innerHTML = descripcionServicio;
+        let colCantidad= document.createElement('td');
+        colCantidad.innerHTML = cantidadServicio;
+        let colValorUnitario = document.createElement('td');
+        colValorUnitario.innerHTML = valorUniServicio;
+        let colValorTotalServicio = document.createElement('td');
+        colValorTotalServicio.innerHTML = valorTotalServicio;
+        let colEditar= document.createElement('td');
+        let btnEditar = document.createElement('button');
+        btnEditar.textContent = 'editar';
+        btnEditar.type = 'button';
+        btnEditar.classList.add('btn');
+        btnEditar.classList.add('btn-info');
+        btnEditar.classList.add('editar-fila');
+        colEditar.appendChild(btnEditar);
+        
+        let colBorrar= document.createElement('td');
+        let btnBorrar = document.createElement('button'); //Estandar y recomendada!!
+        btnBorrar.textContent = 'X';
+        btnBorrar.type = 'button';
+        btnBorrar.classList.add('btn');
+        btnBorrar.classList.add('btn-danger');
+        btnBorrar.classList.add('borrar-fila');
+        colBorrar.appendChild(btnBorrar);
+                    
+        fila.appendChild(colIdServicio);
+        fila.appendChild(colServicio);
+        fila.appendChild(colDescripcion);
+        fila.appendChild(colCantidad);
+        fila.appendChild(colValorUnitario);
+        fila.appendChild(colValorTotalServicio);
+        fila.appendChild(colEditar);
+        fila.appendChild(colBorrar);
+                    
+        tBody.appendChild(fila);
+
+        console.log(fila);
+    })
+})
 
 
 
@@ -26,54 +82,65 @@ buscarForm.addEventListener("submit", async(e) =>{
     if((idServicioBuscar.value!='') || (DescripServicioBuscar.value !='')){
         const services = await getServicios()
         services.forEach(doc =>{
-            const idservicio = doc.id;
-            if(idServicioBuscar.value == idservicio){
-                const nombreServicio = doc.data().servicio;
-                const descripcionServicio = doc.data().descripcion;
-                const cantidadServicio = doc.data().cantidad;
-                const valorUniServicio = doc.data().valorUnitario;
-                const valorTotalServicio = doc.data().valorTotal;
-                console.log(nombreServicio);
-                console.log(descripcionServicio);
-                console.log(cantidadServicio);
-                console.log(valorUniServicio);
-                console.log(valorTotalServicio);
+        const idservicio = doc.id;
+        if(idServicioBuscar.value == idservicio){
+            const nombreServicio = doc.data().servicio;
+            const descripcionServicio = doc.data().descripcion;
+            const cantidadServicio = doc.data().cantidad;
+            const valorUniServicio = doc.data().valorUnitario;
+            const valorTotalServicio = doc.data().valorTotal;
+            console.log(nombreServicio);
+            console.log(descripcionServicio);
+            console.log(cantidadServicio);
+            console.log(valorUniServicio);
+            console.log(valorTotalServicio);
+            let fila = document.createElement('tr');
+            let colIdServicio= document.createElement('td');
+            colIdServicio.innerHTML=idservicio;
+            let colServicio= document.createElement('td');
+            colServicio.innerHTML = nombreServicio;
+            let colDescripcion= document.createElement('td');
+            colDescripcion.innerHTML = descripcionServicio;
+            let colCantidad= document.createElement('td');
+            colCantidad.innerHTML = cantidadServicio;
+            let colValorUnitario = document.createElement('td');
+            colValorUnitario.innerHTML = valorUniServicio;
+            let colValorTotalServicio = document.createElement('td');
+            colValorTotalServicio.innerHTML = valorTotalServicio;
+            let colEditar= document.createElement('td');
+            let btnEditar = document.createElement('button');
+            btnEditar.textContent = 'editar';
+            btnEditar.type = 'button';
+            btnEditar.classList.add('btn');
+            btnEditar.classList.add('btn-info');
+            btnEditar.classList.add('editar-fila');
+            colEditar.appendChild(btnEditar);
+            
+            let colBorrar= document.createElement('td');
+            let btnBorrar = document.createElement('button'); //Estandar y recomendada!!
+            btnBorrar.textContent = 'X';
+            btnBorrar.type = 'button';
+            btnBorrar.classList.add('btn');
+            btnBorrar.classList.add('btn-danger');
+            btnBorrar.classList.add('borrar-fila');
+            colBorrar.appendChild(btnBorrar);
 
-                let fila = document.createElement('tr');
-                let colIdServicio= document.createElement('td');
-                colIdServicio.innerHTML='';
-                let colServicio= document.createElement('td');
-                colServicio.innerHTML = nombreServicio;
-                let colDescripcion= document.createElement('td');
-                colDescripcion.innerHTML = descripcionServicio;
-                let colCantidad= document.createElement('td');
-                colCantidad.innerHTML = cantidadServicio;
-                let colValorUnitario = document.createElement('td');
-                colValorUnitario.innerHTML = valorUniServicio;
-                let colValorTotalServicio = document.createElement('td');
-                colValorTotalServicio.innerHTML = valorTotalServicio;
-                let colEditar= document.createElement('td');
-                colEditar.innerHTML='';
-                let colBorrar= document.createElement('td');
-                colBorrar.innerHTML='';
-                
-                fila.appendChild(colIdServicio);
-                fila.appendChild(colServicio);
-                fila.appendChild(colDescripcion);
-                fila.appendChild(colCantidad);
-                fila.appendChild(colValorUnitario);
-                fila.appendChild(colValorTotalServicio);
-                fila.appendChild(colEditar);
-                fila.appendChild(colBorrar);
-                
-                tBody.appendChild(fila);
+            fila.appendChild(colIdServicio);
+            fila.appendChild(colServicio);
+            fila.appendChild(colDescripcion);
+            fila.appendChild(colCantidad);
+            fila.appendChild(colValorUnitario);
+            fila.appendChild(colValorTotalServicio);
+            fila.appendChild(colEditar);
+            fila.appendChild(colBorrar);
+            
+            tBody.appendChild(fila);
 
-                console.log(fila);
-                }
-
-        })
-        idServicioBuscar.value="";
-    }
+            console.log(fila);
+        }
+    })
+     idServicioBuscar.value="";
+}
 
 
 
