6d987615f786a86817b8a41c49ea15e0efed935f
rusbell ruiz <rusbell.ruiz.p@gmail.com>
2021-10-08 12:49:46 -0500

    desplegar modal para editar un servicio del listado en buscar.html y js

--- servicios/buscar.js
+++ servicios/buscar.js
@@ -1,13 +1,23 @@
 const db = firebase.firestore();
 const idServicioBuscar = document.getElementById('id-servicio');
 const DescripServicioBuscar = document.getElementById('descripcion-servicio');
+const btnGuardarServicio = document.getElementById('btnGuardarServicio');
 const buscarForm = document.getElementById("buscar-form");
 const tablaServicios = document.getElementById('atributos-Servicio');
 const tBody = tablaServicios.querySelector('tbody');
 
+const modal = document.getElementById('ModalServicios');
+const modalServicios = bootstrap.Modal.getOrCreateInstance(modal); //Obtener el objeto de Bootstrap Modal, para poder hacer uso de funciones de utilidad
+const txtDescripcion = document.getElementById('Descripcion');
+const txtServicio = document.getElementById('Servicio');
+const txtValorUnitario = document.getElementById('ValorUnitario');
+const txtEstado = document.getElementById('Estado');
+
+
 //Declaración de variables
 let arrayServicios = [];
 let botonEditandoServicio = null;
+const estado = "disponible";
 
 
 
@@ -15,65 +25,61 @@ const getServicios = () => db.collection('servicios').get();
 
 const buscarservicio = (id) => db.collection("servicios").doc(id).log("si");
 
-window.addEventListener("DOMContentLoaded", async(e) =>{
-    const services = await getServicios()
-    services.forEach(doc =>{
-        const idservicio = doc.id;
-        const nombreServicio = doc.data().servicio;
-        const descripcionServicio = doc.data().descripcion;
-        const cantidadServicio = doc.data().cantidad;
-        const valorUniServicio = doc.data().valorUnitario;
-        const valorTotalServicio = doc.data().valorTotal;
-        console.log(nombreServicio);
-        console.log(descripcionServicio);
-        console.log(cantidadServicio);
-        console.log(valorUniServicio);
-        console.log(valorTotalServicio);
-        let fila = document.createElement('tr');
-        let colIdServicio= document.createElement('td');
-        colIdServicio.innerHTML= idservicio;
-        let colServicio= document.createElement('td');
-        colServicio.innerHTML = nombreServicio;
-        let colDescripcion= document.createElement('td');
-        colDescripcion.innerHTML = descripcionServicio;
-        let colCantidad= document.createElement('td');
-        colCantidad.innerHTML = cantidadServicio;
-        let colValorUnitario = document.createElement('td');
-        colValorUnitario.innerHTML = valorUniServicio;
-        let colValorTotalServicio = document.createElement('td');
-        colValorTotalServicio.innerHTML = valorTotalServicio;
-        let colEditar= document.createElement('td');
-        let btnEditar = document.createElement('button');
-        btnEditar.textContent = 'editar';
-        btnEditar.type = 'button';
-        btnEditar.classList.add('btn');
-        btnEditar.classList.add('btn-info');
-        btnEditar.classList.add('editar-fila');
-        colEditar.appendChild(btnEditar);
-        
-        let colBorrar= document.createElement('td');
-        let btnBorrar = document.createElement('button'); //Estandar y recomendada!!
-        btnBorrar.textContent = 'X';
-        btnBorrar.type = 'button';
-        btnBorrar.classList.add('btn');
-        btnBorrar.classList.add('btn-danger');
-        btnBorrar.classList.add('borrar-fila');
-        colBorrar.appendChild(btnBorrar);
-                    
-        fila.appendChild(colIdServicio);
-        fila.appendChild(colServicio);
-        fila.appendChild(colDescripcion);
-        fila.appendChild(colCantidad);
-        fila.appendChild(colValorUnitario);
-        fila.appendChild(colValorTotalServicio);
-        fila.appendChild(colEditar);
-        fila.appendChild(colBorrar);
-                    
-        tBody.appendChild(fila);
-
-        console.log(fila);
-    })
-})
+ window.addEventListener("DOMContentLoaded", async(e) =>{
+     const services = await getServicios()
+     services.forEach(doc =>{
+         const idservicio = doc.id;
+         const nombreServicio = doc.data().servicio;
+         const descripcionServicio = doc.data().descripcion;
+         const valorUniServicio = doc.data().valorUnitario;
+         arrayServicios.push({
+            id: idservicio,
+            nombreServicio: nombreServicio,
+            descripcionServicio: descripcionServicio,
+            valorUnitario: valorUniServicio
+        });
+         let fila = document.createElement('tr');
+         let colIdServicio= document.createElement('td');
+         colIdServicio.innerHTML= idservicio;
+         let colDescripcion= document.createElement('td');
+         colDescripcion.innerHTML = descripcionServicio;
+         let colServicio= document.createElement('td');
+         colServicio.innerHTML = nombreServicio;
+         let colValorUnitario = document.createElement('td');
+         colValorUnitario.innerHTML = valorUniServicio;
+         let colEstadoServicio = document.createElement('td');
+         colEstadoServicio.innerHTML = "Disponible";
+         let colEditar= document.createElement('td');
+         let btnEditar = document.createElement('button');
+         btnEditar.textContent = 'editar';
+         btnEditar.type = 'button';
+         btnEditar.classList.add('btn');
+         btnEditar.classList.add('btn-info');
+         btnEditar.classList.add('editar-fila');
+         btnEditar.dataset.indice = arrayServicios.length - 1;
+         btnEditar.addEventListener('click', editarServicio);
+         colEditar.appendChild(btnEditar);
+      
+         let colBorrar= document.createElement('td');
+         let btnBorrar = document.createElement('button'); //Estandar y recomendada!!
+         btnBorrar.textContent = 'X';
+         btnBorrar.type = 'button';
+         btnBorrar.classList.add('btn');
+         btnBorrar.classList.add('btn-danger');
+         btnBorrar.classList.add('borrar-fila');
+         colBorrar.appendChild(btnBorrar);
+                   
+         fila.appendChild(colIdServicio);
+         fila.appendChild(colDescripcion);
+         fila.appendChild(colServicio);
+         fila.appendChild(colValorUnitario);
+         fila.appendChild(colEstadoServicio);
+         fila.appendChild(colEditar);
+         fila.appendChild(colBorrar);
+                  
+         tBody.appendChild(fila);
+     })
+ })
 
 
 
@@ -84,29 +90,20 @@ buscarForm.addEventListener("submit", async(e) =>{
         services.forEach(doc =>{
         const idservicio = doc.id;
         if(idServicioBuscar.value == idservicio){
-            const nombreServicio = doc.data().servicio;
             const descripcionServicio = doc.data().descripcion;
-            const cantidadServicio = doc.data().cantidad;
+            const nombreServicio = doc.data().servicio;
             const valorUniServicio = doc.data().valorUnitario;
-            const valorTotalServicio = doc.data().valorTotal;
-            console.log(nombreServicio);
-            console.log(descripcionServicio);
-            console.log(cantidadServicio);
-            console.log(valorUniServicio);
-            console.log(valorTotalServicio);
             let fila = document.createElement('tr');
             let colIdServicio= document.createElement('td');
             colIdServicio.innerHTML=idservicio;
-            let colServicio= document.createElement('td');
-            colServicio.innerHTML = nombreServicio;
             let colDescripcion= document.createElement('td');
             colDescripcion.innerHTML = descripcionServicio;
-            let colCantidad= document.createElement('td');
-            colCantidad.innerHTML = cantidadServicio;
+            let colServicio= document.createElement('td');
+            colServicio.innerHTML = nombreServicio;
             let colValorUnitario = document.createElement('td');
             colValorUnitario.innerHTML = valorUniServicio;
-            let colValorTotalServicio = document.createElement('td');
-            colValorTotalServicio.innerHTML = valorTotalServicio;
+            let colEstadoServicio = document.createElement('td');
+            colEstadoServicio.innerHTML = "Disponible";
             let colEditar= document.createElement('td');
             let btnEditar = document.createElement('button');
             btnEditar.textContent = 'editar';
@@ -114,6 +111,7 @@ buscarForm.addEventListener("submit", async(e) =>{
             btnEditar.classList.add('btn');
             btnEditar.classList.add('btn-info');
             btnEditar.classList.add('editar-fila');
+            btnEditar.addEventListener('click', editarFila);
             colEditar.appendChild(btnEditar);
             
             let colBorrar= document.createElement('td');
@@ -126,22 +124,32 @@ buscarForm.addEventListener("submit", async(e) =>{
             colBorrar.appendChild(btnBorrar);
 
             fila.appendChild(colIdServicio);
-            fila.appendChild(colServicio);
             fila.appendChild(colDescripcion);
-            fila.appendChild(colCantidad);
+            fila.appendChild(colServicio);
             fila.appendChild(colValorUnitario);
-            fila.appendChild(colValorTotalServicio);
+            fila.appendChild(colEstadoServicio);
             fila.appendChild(colEditar);
             fila.appendChild(colBorrar);
             
             tBody.appendChild(fila);
 
-            console.log(fila);
         }
     })
      idServicioBuscar.value="";
 }
 
+})
 
+function editarFila(){
+    alert('El servicio se actualizo correctamente');
+}
 
-    })
+function editarServicio(){
+    let datos = arrayServicios[this.dataset.indice];
+    txtDescripcion.value = datos.descripcionServicio
+    txtServicio.value = datos.nombreServicio;
+    txtValorUnitario.value = datos.valorUnitario;
+    modalServicios.show();
+    modalServicios.show();
+}
+
